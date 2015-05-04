ec86fb0bbc6db23c519f2a5bdb2792725a77bb56
FHorschig <f.horschig@gmail.com>
2015-05-04 12:37:23 +0200

    Annotation for bugs as organisms using dwc standard. Tested but ugly. #1

--- annotations/annotator_test.py
+++ annotations/annotator_test.py
@@ -6,6 +6,9 @@ from helper.toolsprovider import ToolsProvider
 
 
 class TestAnnotator(unittest.TestCase):
+    TEST_IMG = "http://gbif.naturkundemuseum-berlin.de/hackathon/Thumbs/" + \
+               "mfnb_col_buprestidae_julodinae_d011.jpg"
+
     def setUp(self):
         self.tools_provider = ToolsProvider()
         self.annotator = Annotator(self.tools_provider)
@@ -13,6 +16,18 @@ class TestAnnotator(unittest.TestCase):
 
     def test_can_create(self):
         assert self.annotator
+
+
+    def disabled_test_creates_empty_string_without_information(self):
+        assert self.annotator.save_as_turtle() == ""
+
+
+    def test_includes_organism_into_turtle(self):
+        img = TestAnnotator.TEST_IMG
+        self.annotator.add_found_bug(img, 100, 10, 30, 50)
+        self.assertEqual(self.annotator.save_as_turtle(), \
+            "@prefix dwc: <http://rs.tdwg.org/dwc/terms/#> .\n" + \
+            "<" + img + "#x=100&y=10&w=30&h=50> a dwc:Organism .")
 
 
 if __name__ == '__main__':
