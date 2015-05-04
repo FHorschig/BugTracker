1513681f0cee19a37a86e3ee650be7b4221883e2
FHorschig <f.horschig@gmail.com>
2015-05-04 12:37:23 +0200

    Annotation for bugs as organisms using dwc standard. Tested but ugly. #1

--- annotations/annotator.py
+++ annotations/annotator.py
@@ -7,8 +7,21 @@ class Annotator(object):
 
     def __init__(self, tools_provider):
         self.__tools_provider = tools_provider
+        self.__PREFIXES = ["@prefix dwc: <http://rs.tdwg.org/dwc/terms/#> .\n"]
+        self.__bugs = []
 
+    def add_found_bug(self, image_file, x, y, w, h):
+        """Drawn a box around a bug? Tel me with this method"""
+        self.__bugs.append(\
+            "<" + image_file + "#x=" + str(x) + "&y=" + str(y) +\
+            "&w=" + str(w) + "&h=" + str(h) + ">" +\
+            " a dwc:Organism .")
 
-    def save_as_turtle(self):
-        """ESaves the internal data as turtle file."""
-        #TODO(fhorschig): Implement.
+    def save_as_turtle(self, as_string=True):
+        """Saves the internal data as turtle file."""
+        #TODO(fhorschig): Use only needed prefixes.
+        out = "".join(self.__PREFIXES)
+        out = out + "\n".join(self.__bugs)
+        if as_string:
+            return out
+        #TODO(fhorschig): Create file in tools_provider out-dir.
