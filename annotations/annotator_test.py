879a602bdca3b0a49b9e6265768dcd311364b2c9
FHorschig <f.horschig@googlemail.com>
2015-05-04 11:36:27 +0200

    Minor refactorings. #1

--- annotations/annotator_test.py
+++ annotations/annotator_test.py
@@ -2,12 +2,13 @@
 
 import unittest
 from annotator import Annotator
+from bug import Bug
 from helper.toolsprovider import ToolsProvider
 
 
 class TestAnnotator(unittest.TestCase):
-    TEST_IMG = "http://gbif.naturkundemuseum-berlin.de/hackathon/Thumbs/" + \
-               "mfnb_col_buprestidae_julodinae_d011.jpg"
+    BUG = Bug("bug.jpg", 100, 10, 30, 50)
+    PREFIXES = "@prefix dwc: <http://rs.tdwg.org/dwc/terms/#> .\n"
 
     def setUp(self):
         self.tools_provider = ToolsProvider()
@@ -18,16 +19,17 @@ class TestAnnotator(unittest.TestCase):
         assert self.annotator
 
 
-    def disabled_test_creates_empty_string_without_information(self):
-        assert self.annotator.save_as_turtle() == ""
+    def test_creates_prefixes_without_information(self):
+        self.assertEqual( \
+            self.annotator.save_as_turtle(as_string=True), \
+            TestAnnotator.PREFIXES)
 
 
-    def test_includes_organism_into_turtle(self):
-        img = TestAnnotator.TEST_IMG
-        self.annotator.add_found_bug(img, 100, 10, 30, 50)
-        self.assertEqual(self.annotator.save_as_turtle(), \
-            "@prefix dwc: <http://rs.tdwg.org/dwc/terms/#> .\n" + \
-            "<" + img + "#x=100&y=10&w=30&h=50> a dwc:Organism .")
+    def test_includes_added_organism_into_turtle(self):
+        self.annotator.add_bug(TestAnnotator.BUG)
+        self.assertEqual(self.annotator.save_as_turtle(as_string=True), \
+            TestAnnotator.PREFIXES + \
+            TestAnnotator.BUG.as_turtle())
 
 
 if __name__ == '__main__':
