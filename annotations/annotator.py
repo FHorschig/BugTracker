ff5a0de3bcae21b202a89d0f4a9003829817e645
FHorschig <f.horschig@googlemail.com>
2015-05-04 11:36:27 +0200

    Minor refactorings. #1

--- annotations/annotator.py
+++ annotations/annotator.py
@@ -5,22 +5,31 @@ from bug import Bug
 
 class Annotator(object):
     """Provides methods to create annotation files."""
+    __PREFIXES = ["@prefix dwc: <http://rs.tdwg.org/dwc/terms/#> ."]
 
 
     def __init__(self, tools_provider):
         self.__tools_provider = tools_provider
-        self.__PREFIXES = ["@prefix dwc: <http://rs.tdwg.org/dwc/terms/#> .\n"]
         self.__bugs = []
 
-    def add_found_bug(self, image_file, x, y, w, h):
+    def add_bug(self, bug):
         """Drawn a box around a bug? Tel me with this method"""
-        self.__bugs.append(Bug(image_file, x, y, w, h).as_turtle())
+        self.__bugs.append(bug)
 
-    def save_as_turtle(self, as_string=True):
-        """Saves the internal data as turtle file."""
-        #TODO(fhorschig): Use only needed prefixes.
-        out = "".join(self.__PREFIXES)
-        out = out + "\n".join(self.__bugs)
+    def save_as_turtle(self, as_string=False):
+        """Saves the internal data as turtle file or prints it as String."""
         if as_string:
-            return out
-        #TODO(fhorschig): Create file in tools_provider out-dir.
+            return self.__get_prefixes_and_bugs()
+        with open("test.txt", "w") as text_file:
+            text_file.write(self.__get_prefixes_and_bugs())
+
+
+    def __get_prefixes_and_bugs(self):
+        """Returns prefixes and bugs concatenated and ready to save. """
+        return "\n".join(Annotator.__PREFIXES) +\
+               "\n" +\
+               "\n".join(self.__convert_bugs_to_turtle())
+
+    def __convert_bugs_to_turtle(self):
+        """Converts the bug objects into strings."""
+        return [bug.as_turtle() for bug in self.__bugs]
