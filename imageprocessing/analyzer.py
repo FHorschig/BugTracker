32597d8ffda36f833451da9fcacbc5ab8955ca6f
Sten Aechtner <sten.aechtner@student.hpi.uni-potsdam.de>
2015-06-08 13:08:50 +0200

    Output image limits to support hd images

--- imageprocessing/analyzer.py
+++ imageprocessing/analyzer.py
@@ -4,12 +4,13 @@ import cv2
 import numpy
 
 
+max_output_width = 900
+
 class Analyzer(object):
     """Provides methods to extract regions with bugs from images."""
 
-
     def __init__(self, annotator, iohelper):
-        self.result = None
+        self.result_image = None
 
         self.__annotator = annotator
         self.__iohelper = iohelper
@@ -17,14 +18,21 @@ class Analyzer(object):
 
     def process(self, method):
         """Extracts features and stores findings into given annotator."""
-        self.result = method.process(self.__annotator, self.__iohelper)
+        self.result_image = method.process(self.__annotator, self.__iohelper)
 
 
     def show_result(self):
-        if self.result is None:
+        if self.result_image is None:
             return
 
-        if isinstance(self.result, numpy.ndarray):
-            cv2.imshow('Image', self.result)
+        if isinstance(self.result_image, numpy.ndarray):
+            result_image = self.result_image
+
+            width, height = result_image.shape[:2]
+            if width > max_output_width:
+                resize_factor = float(max_output_width) / width
+                result_image = cv2.resize(result_image, (0, 0), fx=resize_factor, fy=resize_factor)
+
+            cv2.imshow('Image', result_image)
             cv2.waitKey()
 
