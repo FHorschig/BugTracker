a782224953918874749ff7af90e081ea31b58a9d
FHorschig <f.horschig@googlemail.com>
2015-06-15 17:56:53 +0200

    Revisited benchmarkfiles and added benchmark file editor for windows.

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -5,7 +5,7 @@ from benchmark import Benchmark
 def execute_all(method, template):
     """ Start a benchmark for the given method for every avaiable file. """
     benchmark = Benchmark(method(), template)
-    benchmark.execute_all(show_images=False)
+    benchmark.execute_all(show_images=True)
     print "Recall: " + str(benchmark.recall())
     print "Precision: " + str(benchmark.precision())
     print "F-Measure: " + str(benchmark.fmeasure())
