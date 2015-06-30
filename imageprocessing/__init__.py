099419592acfc6726694b7db8a340031a1bf638d
Leander Neiß <leander.neiss@student.hpi.uni-potsdam.de>
2015-06-30 16:50:43 +0200

    Add work in progress HOG template Matching

--- imageprocessing/__init__.py
+++ imageprocessing/__init__.py
@@ -1,12 +1,14 @@
 from analyzer import Analyzer
 from thresholding import Thresholding
 from templateMatching import TemplateMatching, TemplateMatchingWithThresholding
+from hogTemplateMatching import HogTemplateMatching
 from qr_detection import QRDetection
 
 
 METHODS = {
     "THRESHOLD": Thresholding,
     "TEMPLATE": TemplateMatching,
+    "HOGTEMPLATE": HogTemplateMatching,
     "QRCODE": QRDetection,
     "TEMPLATE_THRESHOLD": TemplateMatchingWithThresholding,
     "QR_DETECTION": QRDetection,
